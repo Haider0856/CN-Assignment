563a7e5678d094535a174f9dc66ee9a59f17d708
Haider0856 <haider0856@gmail.com>
2025-10-15 04:18:41 +0500

    Resolved merge conflict in byte_stream.hh, kept Check 1 starter code

--- src/byte_stream.hh
+++ src/byte_stream.hh
@@ -12,27 +12,12 @@ class ByteStream
 public:
   explicit ByteStream( uint64_t capacity );
 
-<<<<<<< HEAD
-=======
   // Helper functions (provided) to access the ByteStream's Reader and Writer interfaces
->>>>>>> upstream/check1-startercode
   Reader& reader();
   const Reader& reader() const;
   Writer& writer();
   const Writer& writer() const;
 
-<<<<<<< HEAD
-  void set_error() { error_ = true; };
-  bool has_error() const { return error_; };
-
-protected:
-  uint64_t capacity_;
-  bool error_ {};
-  std::string buffer_ {};
-  uint64_t total_pushed_ = 0;
-  uint64_t total_popped_ = 0;
-  bool is_closed_ = false;
-=======
   void set_error() { error_ = true; };       // Signal that the stream suffered an error.
   bool has_error() const { return error_; }; // Has the stream had an error?
 
@@ -40,43 +25,22 @@ protected:
   // Please add any additional state to the ByteStream here, and not to the Writer and Reader interfaces.
   uint64_t capacity_;
   bool error_ {};
->>>>>>> upstream/check1-startercode
 };
 
 class Writer : public ByteStream
 {
 public:
-<<<<<<< HEAD
-  void push( std::string data );
-  void close();
-
-  bool is_closed() const;
-  uint64_t available_capacity() const;
-  uint64_t bytes_pushed() const;
-=======
   void push( std::string data ); // Push data to stream, but only as much as available capacity allows.
   void close();                  // Signal that the stream has reached its ending. Nothing more will be written.
 
   bool is_closed() const;              // Has the stream been closed?
   uint64_t available_capacity() const; // How many bytes can be pushed to the stream right now?
   uint64_t bytes_pushed() const;       // Total number of bytes cumulatively pushed to the stream
->>>>>>> upstream/check1-startercode
 };
 
 class Reader : public ByteStream
 {
 public:
-<<<<<<< HEAD
-  std::string_view peek() const;
-  void pop( uint64_t len );
-
-  bool is_finished() const;
-  uint64_t bytes_buffered() const;
-  uint64_t bytes_popped() const;
-};
-
-void read( Reader& reader, uint64_t max_len, std::string& out );
-=======
   std::string_view peek() const; // Peek at the next bytes in the buffer -- ideally as many as possible.
   void pop( uint64_t len );      // Remove `len` bytes from the buffer.
 
@@ -86,8 +50,7 @@ void read( Reader& reader, uint64_t max_len, std::string& out );
 };
 
 /*
- * read: A (provided) helper function thats peeks and pops up to `max_len` bytes
+ * read: A (provided) helper function that peeks and pops up to `max_len` bytes
  * from a ByteStream Reader into a string;
  */
 void read( Reader& reader, uint64_t max_len, std::string& out );
->>>>>>> upstream/check1-startercode
