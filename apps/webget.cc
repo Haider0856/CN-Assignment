fda558a4441c3053db5a4443761a65816f71eb26
Haider0856 <haider0856@gmail.com>
2025-10-15 23:01:33 +0500

    Fix: implemented working Reassembler and Webget (check0 + check1 passing)

--- apps/webget.cc
+++ apps/webget.cc
@@ -1,49 +1,49 @@
-#include "debug.hh"
 #include "socket.hh"
-
-#include <cstdlib>
+#include "address.hh"
 #include <iostream>
-#include <span>
 #include <string>
 
 using namespace std;
 
-namespace {
-void get_URL( const string& host, const string& path )
+// Download the resource from the given host and path
+void get_URL(const string &host, const string &path)
 {
-  debug( "Function called: get_URL( \"{}\", \"{}\" )", host, path );
-  debug( "get_URL() function not yet implemented" );
+    // Create and connect the TCP socket
+    TCPSocket socket;
+    Address address(host, "http");
+    socket.connect(address);
+
+    // Send HTTP GET request
+    string request = "GET " + path + " HTTP/1.1\r\n";
+    request += "Host: " + host + "\r\n";
+    request += "Connection: close\r\n\r\n";
+    socket.write(request);
+
+    // Read and print the response
+    string buffer;
+    while (!socket.eof()) {
+        socket.read(buffer);   // <-- note: read into a string reference
+        cout << buffer;
+        buffer.clear();
+    }
+
+    socket.close();
 }
-} // namespace
 
-int main( int argc, char* argv[] )
+// Main function
+int main(int argc, char *argv[])
 {
-  try {
-    if ( argc <= 0 ) {
-      abort(); // For sticklers: don't try to access argv[0] if argc <= 0.
+    if (argc != 3) {
+        cerr << "Usage: " << argv[0] << " HOST PATH\n";
+        return EXIT_FAILURE;
     }
 
-    auto args = span( argv, argc );
-
-    // The program takes two command-line arguments: the hostname and "path" part of the URL.
-    // Print the usage message unless there are these two arguments (plus the program name
-    // itself, so arg count = 3 in total).
-    if ( argc != 3 ) {
-      cerr << "Usage: " << args.front() << " HOST PATH\n";
-      cerr << "\tExample: " << args.front() << " stanford.edu /class/cs144\n";
-      return EXIT_FAILURE;
+    try {
+        get_URL(argv[1], argv[2]);
+    } catch (const exception &e) {
+        cerr << "Exception: " << e.what() << "\n";
+        return EXIT_FAILURE;
     }
 
-    // Get the command-line arguments.
-    const string host { args[1] };
-    const string path { args[2] };
-
-    // Call the student-written function.
-    get_URL( host, path );
-  } catch ( const exception& e ) {
-    cerr << e.what() << "\n";
-    return EXIT_FAILURE;
-  }
-
-  return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
